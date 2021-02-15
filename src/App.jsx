52f8c38a187e060f61342f255448606b0345fcbf
Jay Lowe <jay@makeschool.com>
2021-02-15 22:46:19 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Component1 from './Component1'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Pass a value to the component via props below!</h1>
+      <h5>TODO 1 = define a props value as a attribute of Component1</h5>
+
+      <Component1 />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component1.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+class Component1 extends React.Component {
+    // TODO 2 = call that props value using `this.props.myValueName` wrapped in {}
+    render() {
+        return <h1>Hello from Component1!</h1>
+    }
+}
+
+export default Component1
